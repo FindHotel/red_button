8b6a5fdd16f532288dfb69e08244851b45116233
Elod Peter <bejmuller@gmail.com>
2015-02-13 12:52:36 +0100

    bin version and release notes actions are finished

--- lib/redbutton/git_log_reader.rb
+++ lib/redbutton/git_log_reader.rb
@@ -8,14 +8,9 @@ class GitLogReader
     @repository = Git.open(git_project_path)
     @matchers = config[:matchers]
     @last_message_matcher = config[:last_message_matcher]
-    @source_branch = config[:source_branch]
   end
 
   def read
-    current_branch = @repository.current_branch
-
-    @repository.checkout(@source_branch)
-
     previous_tag_commit = @repository.log(LOG_LIMIT).grep(@last_message_matcher).first
 
     log_range = @repository.log(LOG_LIMIT).between(previous_tag_commit.sha, 'HEAD')
@@ -24,8 +19,6 @@ class GitLogReader
       result[name].concat(log_range.grep(string_matcher).map(&:message))
       result
     end
-
-    @repository.checkout(current_branch)
 
     result
   end
